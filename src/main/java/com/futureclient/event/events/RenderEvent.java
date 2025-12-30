9fe97064b903a3bc8faceb488995aa07ed7bca52
Sujalofficial07 <sujalbaloda32@gmail.com>
2025-12-30 14:35:22 +0530

    Create RenderEvent.java

---
+++ src/main/java/com/futureclient/event/events/RenderEvent.java
@@ -0,0 +1,22 @@
+package com.futureclient.event.events;
+
+import com.futureclient.event.Event;
+import net.minecraft.client.gui.DrawContext;
+
+public class RenderEvent extends Event {
+    private final DrawContext context;
+    private final float tickDelta;
+    
+    public RenderEvent(DrawContext context, float tickDelta) {
+        this.context = context;
+        this.tickDelta = tickDelta;
+    }
+    
+    public DrawContext getContext() {
+        return context;
+    }
+    
+    public float getTickDelta() {
+        return tickDelta;
+    }
+}
