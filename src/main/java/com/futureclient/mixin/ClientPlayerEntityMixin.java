e05d5c4b35c8d210707fa2b084a1cefe1ce3e0c5
Sujalofficial07 <sujalbaloda32@gmail.com>
2025-12-30 14:40:05 +0530

    Create ClientPlayerEntityMixin.java

---
+++ src/main/java/com/futureclient/mixin/ClientPlayerEntityMixin.java
@@ -0,0 +1,9 @@
+package com.futureclient.mixin;
+
+import net.minecraft.client.network.ClientPlayerEntity;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(ClientPlayerEntity.class)
+public class ClientPlayerEntityMixin {
+    // Sprint and movement mixins can be added here
+}
